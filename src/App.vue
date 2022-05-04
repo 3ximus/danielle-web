3d3e92f11378f6deb4b35aa39ba08a92284d1612
eximus <fabio4335@gmail.com>
2022-05-04 00:02:19 -0400

    add sass

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
   <RouterView />
 </template>
 
-<style>
+<style lang="scss">
 @import "@/assets/base.css";
 
 #app {
@@ -65,24 +65,24 @@ nav {
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  a.router-link-exact-active {
+    color: var(--color-text);
+  }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+  a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+  a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
 
-nav a:first-of-type {
-  border: 0;
+  a:first-of-type {
+    border: 0;
+  }
 }
 
 @media (min-width: 1024px) {
