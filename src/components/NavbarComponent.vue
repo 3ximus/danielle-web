1456cb76500b17dbeda9304ecaf89b7255bfb1e2
eximus <fabio4335@gmail.com>
2022-05-07 23:43:58 -0400

    popop

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+export default {};
+</script>
+<template>
+  <h1>Danielle Romaine</h1>
+</template>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+<script lang="ts">
+export default {};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<template>See you later suckers 🖕</template>
