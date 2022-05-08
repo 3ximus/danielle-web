ee764051f9b1d8921e2799c11419648739cb76e2
eximus <fabio4335@gmail.com>
2022-05-08 12:57:42 -0400

    Navbar desktop

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,10 +12,17 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+    },
+    {
+      path: "/works",
+      name: "works",
+      component: () => import("../views/WorksView.vue"),
     },
   ],
 });
