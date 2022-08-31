5ffc4a0574cb83ddc9afdf5a958e3622143a43b9
eximus <fabio4335@gmail.com>
2022-08-31 01:38:25 -0400

    compressed images

--- src/views/home-signs.ts
+++ src/views/home-signs.ts
@@ -1,76 +1,73 @@
 import type { PopSign } from "../components/PopSignComponent.vue";
 
-// TODO for compressing pictures:
-// convert original.png -resize 2000000@> -quality 80 compressed.webp
-
 export const grid: PopSign[][] = [
   [
     // 1st row
     {
-      image: "/pomegranates_botticelli/cover.png",
-      flash: "/pomegranates_botticelli/flash.png",
+      image: "/pomegranates_botticelli/cover.webp",
+      flash: "/pomegranates_botticelli/flash.webp",
     },
     {
-      image: "/fallen_angel/cover.png",
-      flash: "/fallen_angel/flash.png",
+      image: "/fallen_angel/cover.webp",
+      flash: "/fallen_angel/flash.webp",
     },
     {
-      image: "/helios_selene/selene.png",
-      flash: "/helios_selene/sflash.png",
+      image: "/helios_selene/selene.webp",
+      flash: "/helios_selene/sflash.webp",
     },
   ],
   [
     // 2nd row
     {
-      image: "/si_tian_wang/cover.png",
-      flash: "/si_tian_wang/flash.png",
+      image: "/si_tian_wang/cover.webp",
+      flash: "/si_tian_wang/flash.webp",
     },
-    { image: "/stop_horny/cover.png", flash: "/stop_horny/flash.png" },
+    { image: "/stop_horny/cover.webp", flash: "/stop_horny/flash.webp" },
     {
-      image: "/twilight_zone/Twilight Zone.png",
-      flash: "/twilight_zone/Twilight Zone.png",
+      image: "/twilight_zone/Twilight Zone.webp",
+      flash: "/twilight_zone/Twilight Zone.webp",
     },
   ],
   [
     // 3rd row
     {
-      image: "/litter_ahead_lilith/cover.png",
-      flash: "/litter_ahead_lilith/flash.png",
+      image: "/litter_ahead_lilith/cover.webp",
+      flash: "/litter_ahead_lilith/flash.webp",
     },
     {
-      image: "/helios_selene/helios.png",
-      flash: "/helios_selene/hflash.png",
+      image: "/helios_selene/helios.webp",
+      flash: "/helios_selene/hflash.webp",
     },
     {
-      image: "/helios_selene/selene.png",
-      flash: "/helios_selene/sflash.png",
+      image: "/helios_selene/selene.webp",
+      flash: "/helios_selene/sflash.webp",
     },
     {
-      image: "/pomegranates_botticelli/cover.png",
-      flash: "/pomegranates_botticelli/flash.png",
+      image: "/pomegranates_botticelli/cover.webp",
+      flash: "/pomegranates_botticelli/flash.webp",
     },
   ],
   [
     // 4th row
     {
-      image: "/twilight_zone/Twilight Zone.png",
-      flash: "/twilight_zone/Twilight Zone.png",
+      image: "/twilight_zone/Twilight Zone.webp",
+      flash: "/twilight_zone/Twilight Zone.webp",
     },
     {
-      image: "/fallen_angel/cover.png",
-      flash: "/fallen_angel/flash.png",
+      image: "/fallen_angel/cover.webp",
+      flash: "/fallen_angel/flash.webp",
     },
-    { image: "/stop_horny/cover.png", flash: "/stop_horny/flash.png" },
+    { image: "/stop_horny/cover.webp", flash: "/stop_horny/flash.webp" },
   ],
   [
     // 5th row
     {
-      image: "/pomegranates_botticelli/cover.png",
-      flash: "/pomegranates_botticelli/flash.png",
+      image: "/pomegranates_botticelli/cover.webp",
+      flash: "/pomegranates_botticelli/flash.webp",
     },
     {
-      image: "/si_tian_wang/cover.png",
-      flash: "/si_tian_wang/flash.png",
+      image: "/si_tian_wang/cover.webp",
+      flash: "/si_tian_wang/flash.webp",
     },
   ],
 ];
