ceb415ea0ed2de99133c4c1abc549dfbee014ce8
eximus <fabio4335@gmail.com>
2024-11-10 18:40:08 -0500

    good state masonry gallery

--- src/lib/components/GalleryItem.svelte
+++ src/lib/components/GalleryItem.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-	export let src = '';
+	import type { Work } from '$lib/configs/works';
+
+	export let item: Work;
 </script>
 
 <div class="masonry-item">
-	<img class="masonry-content p-2 rounded-2xl" {src} alt="" />
+	<img class="masonry-content p-2 rounded-2xl" src={item.cover} alt="" />
 </div>
+
+<style>
+	img {
+		opacity: 0;
+		transition: opacity 0.4s;
+	}
+</style>
